db6ba256f6cacd9f3168a6a810cecbb9fea3866a
dutake <74233503+dutake@users.noreply.github.com>
2020-11-10 00:22:35 -0800

    I did a fucky wucky and forgot to add <>
    im big stupid

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = class base64encoder extends Plugin {
 		powercord.api.commands.registerCommand({
 			command: 'encode',
 			description: 'It encodes a string from normal text to base 64',
-			usage: '{c} message',
+			usage: '{c} <message>',
 			executor: async (message) => {
 				return await this.stringtobase64(message.join(" "));
 			}
@@ -12,7 +12,7 @@ module.exports = class base64encoder extends Plugin {
 		powercord.api.commands.registerCommand({
 			command: 'decode',
 			description: 'It decodes a string from base 64 to normal text',
-			usage: '{c} message',
+			usage: '{c} <message>',
 			executor: async (message) => {
 				return await this.base64tostring(message.join(" "));
 			}
